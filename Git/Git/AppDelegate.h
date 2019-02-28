f37b7a466949df6f2ed1865c7f13b63f09bd2f34
zhangwei <zhangwei8256@icloud.com>
2019-02-28 09:14:47 +0800

    Create

---
+++ Git/Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by zhangwei on 2019/2/28.
+//  Copyright © 2019 zhangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
